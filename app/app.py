5ef463fd3ea42a3ea08003a940fe5606676efc7a
misha2301 <mishalosik@gmail.com>
2024-12-20 22:16:48 +0500

    Учтены замечания

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+import subprocess
+
+from flask import Flask, render_template, request, jsonify
 import sqlite3
 
 app = Flask(__name__)
@@ -6,7 +8,7 @@ app = Flask(__name__)
 
 def get_drinks(sort_by=None,order = None, search = None):
     # Подключаемся к базе данных
-    conn = sqlite3.connect('drinks.db')
+    conn = sqlite3.connect('../drinks.db')
     cursor = conn.cursor()
 
     # Формируем SQL-запрос
@@ -16,13 +18,9 @@ def get_drinks(sort_by=None,order = None, search = None):
     if search:
         query += f" WHERE LOWER(name) LIKE ('%{search.lower()}%')"
 
-    # Добавляем сортировку, если указано
     if sort_by:
-        query += f" ORDER BY {sort_by}"
-        if order == "desc":
-            query += " DESC"
-        elif order == "asc":
-            query += " ASC"
+        query += f" ORDER BY {sort_by} {order.upper() if order else ''}"
+    cursor.execute(query)
 
     cursor.execute(query)
     drinks = cursor.fetchall()
@@ -38,6 +36,15 @@ def index():
     order = request.args.get('order')  # Получаем параметр порядка сортировки (по возрастанию или убыванию)
     drinks = get_drinks(sort_by=sort_by, order=order, search=search_query)  # Получаем напитки из базы
     return render_template('index.html', drinks=drinks)
+
+@app.route('/scrape', methods=['POST'])
+def scrape():
+    try:
+        # Запускаем скрипт скрапинга
+        subprocess.run(["python", "scrapping.py"], check=True)
+        return jsonify({"message": "Скрапинг успешно завершён!"}), 200
+    except subprocess.CalledProcessError as e:
+        return jsonify({"message": "Ошибка при запуске скрапинга!", "error": str(e)}), 500
 
 
 if __name__ == '__main__':
